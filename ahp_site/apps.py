a9935b2f6a7c84473fee4fe29fa76296676b8ea3
Stephen Schlie <heschlie@gmail.com>
2016-03-09 21:04:49 -0800

    initital commit

---
+++ ahp_site/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AhpSiteConfig(AppConfig):
+    name = 'ahp_site'
---
+++ ahp_site/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class AhpUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    number_of_reports = models.IntegerField()
+    submitted_reports = None
+    submitted_comments = None
+
+
+class ReportModel(models.Model):
+    title = models.CharField(max_length=255)
+    picture = None
+    photos = None
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    violation = models.CharField(max_length=255)
+    submit_date = models.DateTimeField(auto_now_add=True)
+    reporter = models.ForeignKey(AhpUser, on_delete=models.CASCADE)
+    votes = None
